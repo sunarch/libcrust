cb3d7c566f8274240bd2fa0be95df5b14c550ffa
András Németh <sunarch@protonmail.com>
2020-10-15 19:46:50 +0200

    Change license to Mozilla Public License 2.0

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # libcrust - Shell collection
 
 Shell libraries, utilities, snippets, etc.
+
+### License
+[Mozilla Public License 2.0](https://spdx.org/licenses/MPL-2.0.html)
--- unsorted/ansi-escape-sequences.sh
+++ unsorted/ansi-escape-sequences.sh
@@ -1,5 +1,10 @@
 # ANSI escape sequences #
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 # variants of the escape code character: #######################################
 
 # shell                   (e) - (for "escape") : example ('\e[0m')
